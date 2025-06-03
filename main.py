7308317dbfc6300cc09d0ad8260449e3b01a58e8
shinyEazy <tanhngusi39@gmail.com>
2025-06-03 16:42:18 +0700

    feat

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from langchain_text_splitters import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter, TokenTextSplitter, CharacterTextSplitter
 
-with open('data/data_uet_vnu.txt', 'r', encoding='utf-8') as file:
+with open('input.md', 'r', encoding='utf-8') as file:
     document = file.read()
 
+# text_splitter = TokenTextSplitter.from_tiktoken_encoder(
+#     encoding_name="cl100k_base", chunk_size=512, chunk_overlap=51
+# )
+# texts = text_splitter.split_text(document)
+
 text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
-    encoding_name="cl100k_base", chunk_size=1000, chunk_overlap=100
+    encoding_name="cl100k_base", chunk_size=512, chunk_overlap=51
 )
 texts = text_splitter.split_text(document)
 
-with open('data/chunked_data.txt', 'w', encoding='utf-8') as output_file:
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=51)
+# texts = text_splitter.split_text(document)
+
+with open('output.txt', 'w', encoding='utf-8') as output_file:
     for i, text in enumerate(texts, 1):
         output_file.write(f"Chunk {i}:\n{text}\n\n")
 
-print("Results have been saved to data/chunked_data.txt")
+print(len(texts))
+print("Results have been saved.")
